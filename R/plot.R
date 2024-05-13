72f3d756905d74203632e6cdfde4d0ad29df45be
Lukas D Sauer <sauer@imbi.uni-heidelberg.de>
2024-05-13 15:09:14 +0200

    Small fix for plot_weights

--- R/plot.R
+++ R/plot.R
@@ -3,7 +3,7 @@ plot_weights.fujikawa_x <- function(design, n, r1, weight_fun,
   if(is.null(design$design_exact)){
     design <- set_design_exact(design)
   }
-  return(baskexact::plot(design = design$design_exact, n = n, r1 = r1,
+  return(baskexact::plot_weights(design = design$design_exact, n = n, r1 = r1,
                          weight_fun = weight_fun,
                          weight_params = weight_params))
 }
